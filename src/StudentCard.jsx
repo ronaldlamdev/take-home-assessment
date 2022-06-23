be002f707da110d6e596ae66ff3754dd2cce4ef9
milessteamaccount <ronaldlamdev@gmail.com>
2022-06-23 13:07:35 -0500

    styled divs

--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -11,11 +11,16 @@ const studentCard = ({student}) => {
   return (
   <div className="student-card">
     <img className="student-image" src={student.pic} alt="pic"/>
-    <h1 className="student-name">{student.firstName} {student.lastName}</h1>
-    <p className="email">Email: {student.email}</p>
-    <p className="company-name">Company: {student.company}</p>
-    <p className="skill">Skill: {student.skill}</p>
-    <p className="average">Average: <AvgGrade/>%</p>
+    <div className="text-container">
+      <h1 className="student-name">{student.firstName} {student.lastName}</h1>
+      <div className="more-info-container">
+        <p className="email">Email: {student.email}</p>
+        <p className="company-name">Company: {student.company}</p>
+        <p className="skill">Skill: {student.skill}</p>
+        <p className="average">Average: <AvgGrade/>%</p>
+      </div> 
+    </div>
+    
   </div>
   )
 };
