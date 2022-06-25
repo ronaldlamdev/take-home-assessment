2342002c6b14f55b93024a086a9903b846e3ab65
milessteamaccount <ronaldlamdev@gmail.com>
2022-06-25 15:59:04 -0500

    created test grades list

--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -18,9 +18,18 @@ const studentCard = ({student}) => {
         <p className="company-name">Company: {student.company}</p>
         <p className="skill">Skill: {student.skill}</p>
         <p className="average">Average: <AvgGrade/>%</p>
+        <ul className="test-grades-list">
+          <li>Test 1: <span>{student.grades[0]}%</span></li>
+          <li>Test 2: <span>{student.grades[1]}%</span></li>
+          <li>Test 3: <span>{student.grades[2]}%</span></li>
+          <li>Test 4: <span>{student.grades[3]}%</span></li>
+          <li>Test 5: <span>{student.grades[4]}%</span></li>
+          <li>Test 6: <span>{student.grades[5]}%</span></li>
+          <li>Test 7: <span>{student.grades[6]}%</span></li>
+          <li>Test 8: <span>{student.grades[7]}%</span></li>
+        </ul> 
       </div> 
     </div>
-    
   </div>
   )
 };
